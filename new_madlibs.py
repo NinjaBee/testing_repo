d7085095c8e328d09510e97e84423adeead7ce7f
ninjabee <bkharris77@gmail.com>
2020-02-26 22:49:24 -0800

    Cleaning up test function.

---
+++ new_madlibs.py
@@ -0,0 +1,84 @@
+
+
+# How many nouns: 12
+# How many plural nouns: 2
+# How many verbs: 2
+# Verb past tense: 2
+# Verb - ing: 3
+# How many Adjectives: 5 
+# How many body parts: 2 
+# City Name: 1
+# Direction: 1 
+
+nouns = []
+plural_nouns = []
+verbs = []
+past_verbs = []
+ing_verbs = []
+adjectives = []
+body_parts = []
+
+city_name = input('Name a city you know ...or one from Harry Potter.')
+direction = input('State a direction... eg. Left, South...')
+
+x = 0
+
+def insert_noun():
+    for noun in nouns
+        this_noun = nouns[x]
+    x += 1
+    return this_noun
+
+
+# Harry Potter MadLib #1 
+# (by Elanor Gamgee )
+
+# Taken from Chapter Five, "The Whomping Willow" in Harry Potter and the Chamber of Secrets
+
+ 
+
+# "Check that no one's (-ing verb) ," said Ron, starting the
+
+# (noun) with another tap of his (noun) .
+
+# Harry stuck his (body part) out of the (noun) :
+
+# (noun) was fumbling along the main
+
+# (noun) ahead, but their street was (adjective) .
+
+# "Okay," he said.
+
+# Ron pressed a (adjective) silver (noun) on the
+
+# (noun) . The (noun) around them
+
+# (verb, past tense) -- and so did they. Harry could feel
+
+# the (noun) (-ing verb) beneath him,
+
+# (verb) the (noun) ,
+
+# feel his (body part) on his knees and his
+
+# (noun) on his nose, but for all he could
+
+# (verb) , he had become a pair of
+
+# (plural noun) , (-ing verb)
+
+# a few feet above the ground in a dingy street full of parked cars.
+
+# "Let's go," said Ron's voice from his (direction) .
+
+# And the (noun) and the (adjective)
+
+# (plural noun) on either side
+
+# (verb - past tense) away,
+
+# (-ing verb) out of site as the (noun) rose;
+
+# in seconds, the whole of (city name) lay,
+
+# (adjective) and (adjective) below them.'
