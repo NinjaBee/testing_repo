ad37e65731a78f1ecabef8f578f1ce8854ae12f6
ninjabee <bkharris77@gmail.com>
2020-02-27 18:54:32 -0800

    redoing madlib lab as repeatable madlib(s) that use regex to find and replace nouns, etc. with user input based on the part of speach/object needed.

---
+++ new_madlibs02.py
@@ -0,0 +1,11 @@
+import random 
+import re 
+# import os 
+#import time  -- Will use this later if time to pause before display/time out "waiting" ascii art, etc. 
+
+madlib_text =  "Harry Potter MadLib #1 (by Elanor Gamgee )\n Taken from Chapter Five, 'The Whomping Willow'\n in Harry Potter and the Chamber of Secrets\n 'Check that no one's (-ing verb) ,' said Ron, starting the\n (noun) with another tap of his (noun).\n Harry stuck his (body part) out of the (noun) :\n (noun) was fumbling along the main\n (noun) ahead, but their street was (adjective).\n 'Okay,' he said.\n Ron pressed a (adjective) silver (noun) on the\n (noun) . The (noun) around them (verb, past tense) -- and so did they.\n Harry could feel the (noun) (-ing verb) beneath him,\n (verb) the (noun), feel his (body part) on his knees and his\n (noun) on his nose, but for all he could (verb) , he had become a pair of\n (plural noun) , (-ing verb) a few feet above the ground in a dingy street\n full of parked cars. 'Let's go,' said Ron's voice from his (direction) .\n And the (noun) and the (adjective) (plural noun) on either side (verb - past tense) away,\n (-ing verb) out of site as the (noun) rose; in seconds, the whole of (city name) lay,\n (adjective) and (adjective) below them."
+
+
+
+
+print(madlib_text)
