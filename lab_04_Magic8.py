90b769dc52465b920e29e11d089e5903138b3b9a
ninjabee <bkharris77@gmail.com>
2020-03-03 22:25:02 -0800

    adjusted timing a little.

--- lab_04_Magic8.py
+++ lab_04_Magic8.py
@@ -11,18 +11,23 @@ def main():
     running = True 
 
     while running == True: 
+        # The "\x1b[1;37;40m" bit is for adjusting how the text displays in the terminal.
         get_answer = input("\x1b[1;37;40m" + 'Ready to have your future told? \n \
             \n If you would like to ask the 8 ball a question, type "ask"\n \
             \n If you would like to think and come agian another day type "q."\n:').lower()
         
         if get_answer == 'ask':
             question = input('Meditate on your question. Do you have it? When you are ready type "y" and hit enter....\n:')
+            # time.sleep is being used in this block to add rhythm for the user.
             time.sleep(1)
+            #Text color is being altered for the answer. I also decided to make answers upper case here. 
             print("\n \x1b[1;35;40m" +random.choice(magic_answers).upper() + "\x1b[1;37m \n" )
             time.sleep(2)
 
         elif get_answer != 'ask':
-            print("Yeah...I wouldn't want to know the answer either.\n \n ")
+            print("Yeah...")
+            time.sleep(2)
+            print("I wouldn't want to know the answer either...\n \n ")
             break
 
 main()
