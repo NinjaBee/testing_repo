8f71bdea5cce19c6b8f796263d377854d3c10a11
ninjabee <bkharris77@gmail.com>
2020-03-05 22:17:48 -0800

    timing, correcting while True

--- lab_10_avergage_num.py
+++ lab_10_avergage_num.py
@@ -1,20 +1,25 @@
 # This lab takes a user created list of numbers and gives back the average of numbers entered. 
+import time
 
 numbers = []
 
-keep_adding == True 
 
-while keep_adding == True:
+while True:
     add_number = input("What number would you like to add to the list? If you do not wish to add any more, type 'done.'").lower()
     if add_number == "done":
-        keep_adding = False 
+        break
     else:
-        keep_adding = True 
-    add_number = int(add_number)
-    numbers.append(add_number)
+        add_to_list = int(add_number)
+        numbers.append(add_to_list)
 
 list_sum = sum(numbers)
 list_length = int(len(numbers))
 
-print(f"Your list was {str(list_length)} items long. All the items added together came out to {str(list_sum)} with the average number being {str(list_sum / list_length)}.")
+time.sleep(3)
+print(f"\nYour list was {str(list_length)} items long.\n")
+time.sleep(0.5)
+print(f"All the items added together came out to {str(list_sum)}...\n"
+time.sleep(2)
+print(f"with the average number being {str(list_sum / list_length)}.\n")
+
 
