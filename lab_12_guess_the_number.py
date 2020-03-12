933a9366b32d16d2dd9f0539fc60321d3b8ba7f2
Brianna Harris <bkharris77@gmail.com>
2020-03-11 21:39:17 -0700

    minor adjustments to while loop

--- lab_12_guess_the_number.py
+++ lab_12_guess_the_number.py
@@ -4,9 +4,9 @@ computer_choice = random.randint(1,10)
 
 guess_count = 0 
 
-while guess_count <= 0:
+while guess_count <= 10:
     while True:
-        user_guess = input("Guess a number between 1 and 10.  For example '6.'/n:")
+        user_guess = input("Guess a number between 1 and 10.  For example '6.'\n:")
         try: 
             guess_num = int(user_guess)
             #return guess_num
@@ -14,17 +14,15 @@ while guess_count <= 0:
         except TypeError:
             print("Oops! Please only enter numerals. :-) ")
 
-    guess_count += 1 
-
     if guess_num == computer_choice:
         print(f"You're right! The computer selected {str(computer_choice)}. You guessed {str(guess_count)} times!")
         break
     elif guess_num > computer_choice:
         print("Oops! That was a little too high!")
-        pass
     elif guess_num < computer_choice:
         print("Sorry! That one was too low...")
-        pass
+
+    guess_count += 1 
 
     
 
